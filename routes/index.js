72c30ab973235ae4f28725476ba5cac9926aed48
Mateusz Drażyk <drazyk.mateusz@gmail.com>
2017-09-02 14:23:45 +0200

    added paged for fetching campaigns

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const voucherifyClient = require('voucherify');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.post('/campaigns', (req, res, next) => {
+  const client = voucherifyClient({
+    applicationId: req.body.applicationId,
+    clientSecretKey: req.body.secretKey
+  });
+
+  client.campaigns.list()
+  .then(data => data.campaigns)
+  .then(data => res.render('campaigns', {campaigns: data}))
+  .catch(() => res.render('index'));
 });
 
 module.exports = router;
