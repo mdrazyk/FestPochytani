4855804a11e7bd2913b4eecd04f7e96eb9c28df1
Mateusz Drażyk <drazyk.mateusz@gmail.com>
2017-09-02 17:01:37 +0200

    added landing page gen

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,29 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/campaigns', (req, res, next) => {
+  const applicationId = req.body.applicationId;
+  const clientSecretKey = req.body.secretKey;
+  
   const client = voucherifyClient({
-    applicationId: req.body.applicationId,
-    clientSecretKey: req.body.secretKey
+    applicationId: applicationId,
+    clientSecretKey: clientSecretKey
   });
 
   client.campaigns.list()
   .then(data => data.campaigns)
   .then(data => {
-    res.render('campaigns', {campaigns: data});
+    res.render('campaigns', {campaigns: data, keys: [applicationId, clientSecretKey]});
   })
   .catch(() => res.render('index'));
 });
+
+router.get('/campaigns/:id', (req, res, next) => {
+  res.render('landingPageLayout', {title: req.params.id});
+});
+
+// router.post('/landingPageLayout/:id/updateUser', (req, res, next) => {
+
+//   console.log(req.body);
+// });
 
 module.exports = router;
