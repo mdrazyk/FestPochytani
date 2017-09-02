57a92c599d3d046093c60669c947a2dede7ee686
System <none>
2017-09-02 15:01:34 +0000

    ⛽️👻 Updated with Glitch

--- public/scripts/adGenerator.js
+++ public/scripts/adGenerator.js
@@ -8,8 +8,35 @@ documentReady.then(() => {
   else{
     window.document.getElementById('fbLogin').style.display='none'
   }
+  const campaignName = getParameterByName('campaignName')
+  campaignName && window.localStorage.setItem('campaignName', campaignName)
 })
-const setFbApplicationId = function(e){
+
+const setFbApplicationId = () =>{
   var applicationId = window.document.getElementById('applicationId').value
-  window.localStorage.setItem('fbApplicationId', applicationId)
+  window.localStorage.setItem('applicationId', applicationId)
+}
+
+const getCampaign = name => Promise.resolve({
+  
+})
+
+const createAd = () => {
+  const name = window.localStorage.getItem('campaignName')
+  console.log(name)
+  getCampaign(name).then({
+    fetch
+  })
+}
+
+
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
