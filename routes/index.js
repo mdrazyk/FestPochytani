13de7710d9ccfab3cf788837ff19f83894fe24dc
Bartłomiej Glac <Bartomiej Glac>
2017-09-02 15:03:41 +0200

    Added adgenerator :

--- routes/index.js
+++ routes/index.js
@@ -14,9 +14,30 @@ router.post('/campaigns', (req, res, next) => {
   });
 
   client.campaigns.list()
-  .then(data => data.campaigns)
-  .then(data => res.render('campaigns', {campaigns: data}))
-  .catch(() => res.render('index'));
+    .then(data => data.campaigns)
+    .then(data => res.render('campaigns', { campaigns: data }))
+    .catch(() => res.render('index'));
 });
+
+router.get('/adgenerator', (req, res, next) => {
+  const campaign = {
+    name: 'redemption-limit',
+    type: 'AUTO_UPDATE',
+    description: null,
+    start_date: null,
+    expiration_date: null,
+    metadata: null,
+    vouchers_generation_status: 'DONE',
+    voucher:
+    {
+      type: 'DISCOUNT_VOUCHER',
+      is_referral_code: false
+    },
+    object: 'campaign'
+  }
+
+  res.render('adgenerator')
+
+})
 
 module.exports = router;
