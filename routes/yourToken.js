f3d49689724c417133844b035d7c09cbed000367
tarnas14 <tarnas14@gmail.com>
2017-09-02 16:42:35 +0200

    added tracking based on campaign metadata

--- routes/yourToken.js
+++ routes/yourToken.js
@@ -10,10 +10,40 @@ router.get('/', function(req, res, next) {
       clientSecretKey: app_secret
   })
 
-  const publishVoucherPromise = voucherifyClient.distributions.publish(campaign_name)
+  voucherifyClient.campaigns.get(campaign_name)
+    .then(campaignResponse => {
+      if(!campaignResponse.metadata.face2.track) {
+        const publishVoucherPromise = voucherifyClient.distributions.publish(campaign_name)
 
-  publishVoucherPromise.then(publishedVoucher => res.render('yourToken', {
-    title: `Your ${campaign_name} token`,
+        publishVoucherPromise.then(publishedVoucher => res.render('yourToken', {
+          title: `Your ${campaign_name} token`,
+          code: publishedVoucher.voucher.code,
+          face2: publishedVoucher.voucher.metadata.face2
+        }))
+
+        return
+      }
+
+      res.render('getYourToken', {
+        title: `Receive your ${campaign_name} token here`,
+        face2: campaignResponse.metadata.face2
+      })
+    })
+})
+
+router.get('/getCode', (req, res, next) => {
+  const {app_id, app_secret, campaign_name, email} = req.query
+
+  const voucherifyClient = require('voucherify')({
+      applicationId: app_id,
+      clientSecretKey: app_secret
+  })
+
+  voucherifyClient.distributions.publish(campaign_name, {
+    customer: {
+      source_id: email
+    }
+  }).then(publishedVoucher => res.status(200).json({
     code: publishedVoucher.voucher.code,
     face2: publishedVoucher.voucher.metadata.face2
   }))
